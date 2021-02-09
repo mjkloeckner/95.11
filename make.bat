df07e5f9d8d5ca82b0b4c1c1a20829c505cf84e3
Martin J. Klöckner <64109770+klewer-martin@users.noreply.github.com>
2021-02-09 17:17:35 -0300

    Added make file for windows;
    The file is a port of the Makefile to a batch script;

---
+++ make.bat
@@ -0,0 +1,12 @@
+SET CC=gcc
+SET CFLAGS=-std=c99 -Wall -pedantic
+
+
+echo %CFLAGS%
+%CC% %CFLAGS% -c print_file.c
+%CC% %CFLAGS% -c read_file.c
+%CC% %CFLAGS% -c load_country_codes.c
+%CC% %CFLAGS% -c perrors.c
+%CC% %CFLAGS% -c arguments.c
+%CC% %CFLAGS% -c main.c
+%CC% %CFLAGS% main.o arguments.o perrors.o load_country_codes.o read_file.o print_file.o -o main
