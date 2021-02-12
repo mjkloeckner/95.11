067c406f759a2b8c476dff45f68bdd7d70ef41ad
klewer-martin <martin.cachari@gmail.com>
2021-02-12 17:27:34 -0300

    Update;

--- guia07/ex03.c
+++ guia07/ex03.c
@@ -2,14 +2,16 @@
 
 int main (void)
 {
-	unsigned int a, b, c, d;
+	unsigned int aux, a, b, c, d;
 
 	a = 0157;
 	b = 0701;
 	c = 0673;
 	d = 067;
-	
+	aux = 0b1101111;
+
 	printf("%o\n%o\n%o\n%o\n", a<<1, b<<2, c>>2, d>>3);
+	printf("%o\n", aux<<1);
 
 	return 0;
 
