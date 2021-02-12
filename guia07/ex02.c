37cb29600e24d0fdad5a46283083903b1cce12fa
klewer-martin <martin.cachari@gmail.com>
2021-02-12 16:58:37 -0300

    Update: Added ex02.c & ex03.c

---
+++ guia07/ex02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void) 
+{
+	unsigned char a, b;
+
+	a = 0b11001010;
+	b = 0b10100101;
+
+	printf("a = %o\nb = %o\n", a, b);
+
+
+	return 0;
+}
---
+++ guia07/ex03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+	unsigned int a, b, c, d;
+
+	a = 0157;
+	b = 0701;
+	c = 0673;
+	d = 067;
+	
+	printf("%o\n%o\n%o\n%o\n", a<<1, b<<2, c>>2, d>>3);
+
+	return 0;
+
+}
