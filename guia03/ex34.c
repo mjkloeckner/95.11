706b793a291556587d1be8adff14d6bb3d6ce5a9
klewer-martin <martin.cachari@gmail.com>
2021-03-23 18:28:27 -0300

    Update: ex34.c;

--- guia03/ex34.c
+++ guia03/ex34.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-#define N_MAX 10
+#define N 10
+
+bool isSorted(int *vector, size_t size);
 
 int main (void) 
 {
-	int vector[N];
+	int vector[N] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-			
-
+	printf("The vector is sorted: %s\n", isSorted(vector, N) ? "Yes" : "No");
 
 	return 0;
 }
+
+bool isSorted(int *vector, size_t size)
+{
+	size_t i;
+	for(i = 1; (i < size) && (vector[i] >= vector[i - 1]); i++)
+		;
+
+	return true * (size == i);
+}
+
