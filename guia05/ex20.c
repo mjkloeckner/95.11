2bfa2598184aed7ee361af6b3842bae0bf82085f
klewer-martin <martin.cachari@gmail.com>
2021-04-06 23:30:19 -0300

    Update: added ex20.c;

---
+++ guia05/ex20.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef enum {
+	OK,
+	ERROR_NULL_POINTER
+} status_t;
+
+status_t string_reverse(char *str);
+status_t swap_char(char *a, char *b);
+
+int main (void)
+{
+	char str[] = "Hello world!";
+
+	puts(str);
+	string_reverse(str);
+	puts(str);
+
+	return 0;
+}
+
+status_t string_reverse(char *str)
+{
+	if(str == NULL) return ERROR_NULL_POINTER;
+
+	size_t len = strlen(str);
+	for(size_t i = 0; i < (len / 2); i++)
+		swap_char(&str[i], &str[len - 1 - i]);
+
+	return OK;
+}
+
+void swap_char(char *a, char *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
